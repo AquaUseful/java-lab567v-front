c6c436320f0f4312b76b22699b47b06325b7a48b
AquaUseful <l.vladik.m@gmail.com>
2023-02-01 23:25:20 +0300

    add: Some constants

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,3 +1,5 @@
 export const displayNoneClass = "d-none";
 export const popoverHiddenEventName = "hidden.bs.popover";
 export const popoverShownEventName = "shown.bs.popover";
+export const userSelfUrl = "/api/user/self";
+export const userSelfAvatarUrl = "/api/user/self/avatar"
