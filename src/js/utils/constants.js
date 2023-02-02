6c3cf398de4328aa0fff0328bc4a281202258cda
AquaUseful <l.vladik.m@gmail.com>
2023-02-02 13:54:47 +0300

    add: Constant to get orders

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -3,3 +3,4 @@ export const popoverHiddenEventName = "hidden.bs.popover";
 export const popoverShownEventName = "shown.bs.popover";
 export const userSelfUrl = "/api/user/self";
 export const userSelfAvatarUrl = "/api/user/self/avatar"
+export const orderSelfListUrl = "/api/order/self";
