f6c371326c476e1662f14ce227d21bd02f6463c4
AquaUseful <l.vladik.m@gmail.com>
2023-02-02 17:06:14 +0300

    add: User list url constant

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -4,3 +4,4 @@ export const popoverShownEventName = "shown.bs.popover";
 export const userSelfUrl = "/api/user/self";
 export const userSelfAvatarUrl = "/api/user/self/avatar"
 export const orderSelfListUrl = "/api/order/self";
+export const userListUrl = "/api/user"
